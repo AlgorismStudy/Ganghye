8d7c76d21f1015437e0463630cd337168d37cbc4
ganghye <kassia3160@gmail.com>
2021-12-28 19:45:58 +0900

    2021-12-28

---
+++ 2839.py
@@ -0,0 +1,22 @@
+def main():
+    print(solution())
+
+
+def solution():
+    N = int(input())
+    answer = 0
+
+    while N >= 0:
+        sugar_5 = N // 5
+        if N % 5 == 0:
+            answer += sugar_5
+            break
+        N -= 3
+        answer += 1
+    else:
+        answer = -1
+    return answer
+
+
+if __name__ == "__main__":
+    main()
