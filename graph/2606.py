3c3e5f7d8015d5b1c63036cf30953f790858f77c
ganghye <kassia3160@gmail.com>
2022-01-03 23:34:38 +0900

    20220103

--- graph/2606.py
+++ graph/2606.py
@@ -3,24 +3,30 @@ from collections import deque
 N = int(input())
 line_N = int(input())
 graph = dict()
+
 for i in range(line_N):
     a, b = map(int, input().split())
     if a not in list(graph):
         graph[a] = [b]
     else:
         graph[a] += [b]
-print(graph)
+
+    if b not in list(graph):
+        graph[b] = [a]
+    else:
+        graph[b] += [a]
+
 
 q = deque()
-q.append(1)
 visited = [False] * (N + 1)
+q.append(1)
+
 while q:
     curr = q.popleft()
-    if curr not in graph.keys():
-        continue
-    for i in graph[curr]:
-        if not visited[curr]:
-            q.append(graph[curr].pop())
-            visited[i] = True
-print(graph)
-print(visited)
+    if graph[curr]:
+        for i in graph[curr]:
+            if not visited[i]:
+                q.append(i)
+                visited[i] = True
+
+print(visited.count(True) - 1)
