3eb5050171352542341ca95e5def9448dfc895bd
ganghye <kassia3160@gmail.com>
2022-01-07 21:25:54 +0900

    20220107

--- hasing/4195.py
+++ hasing/4195.py
@@ -1,24 +1,26 @@
 N = int(input())
 
 
+def dfs(v, visited, d):
+    cnt = 1
+    visited[v] = True
+    for i in d[v]:
+        if not visited[i]:
+            cnt += 1
+            dfs(i, visited, d)
+    return cnt
+
+
 for _ in range(N):
     M = int(input())
-    s = set()
     d = dict()
+
     for _ in range(M):
-
         per1, per2 = input().split()
-        if per1 in s or per2 in s:
-            d[per1] += 1
-            d[per1] += 1
-        if per2 in s:
-            d[per2] += 1
+        if per1 in d:
+            d[per1] += [per2]
+        else:
+            d[per1] = [per2]
 
-        if per1 not in s:
-            d[per1] = 1
-            s.add(per1)
-        if per2 not in s:
-            d[per2] = 1
-            s.add(per2)
-        print(d)
-        print(s)
+        visited = [False] * len()
+        print(dfs(per1, visited, d))
