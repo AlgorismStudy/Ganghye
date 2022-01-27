0ccae38cfe3b8a711a96054f8f2daf2cf78748e1
ganghye <kassia3160@gmail.com>
2022-01-27 22:20:14 +0900

    20220127

---
+++ book/예제/10_도시분할.py
@@ -0,0 +1,35 @@
+def find(parent, a):
+    if a != parent[a]:
+        parent[a] = find(parent, parent[a])
+    return parent[a]
+
+
+def union(parent, a, b):
+    a = find(parent, a)
+    b = find(parent, b)
+    if a < b:
+        parent[b] = a
+    else:
+        parent[a] = b
+
+
+n, m = map(int, input().split())
+graph = []
+parent = [i for i in range(n+1)]
+
+for i in range(m):
+    a, b, c = map(int, input().split())
+    graph.append((a, b, c))
+
+graph = sorted(graph, key=lambda x: x[2])
+result = 0
+last = 0
+
+for a, b, c in graph:
+    a = find(parent, a)
+    b = find(parent, b)
+    if a != b:
+        union(parent, a, b)
+        result += c
+        last = c
+print(result - last)
---
+++ book/예제/10_팀결성.py
@@ -0,0 +1,29 @@
+def find(x):
+    if x != parent[x]:
+        parent[x] = find(parent[x])
+    return parent[x]
+
+
+def union(x, y):
+    x = find(x)
+    y = find(y)
+    if x > y:
+        parent[x] = y
+    else:
+        parent[y] = x
+
+
+n, m = map(int, input().split())
+arr = []
+parent = [i for i in range(0, n+1)]
+
+
+for _ in range(m):
+    a, b, c = map(int, input().split())
+    if a == 0:
+        union(b, c)
+    else:
+        if find(b) == find(c):
+            print("YES")
+        else:
+            print("NO")
